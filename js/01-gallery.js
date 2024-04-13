d5ed42d7f70dd859e614efbcca6bba5f0d87cbcd
Sylwia Matusik-Slomka <sylwia.matusik.1994@gmail.com>
2024-04-13 21:29:52 +0200

    Update added base mechanics

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+console.log(galleryItems);
+
+const listGallery = document.querySelector(".gallery");
+
+galleryItems.forEach((image) => {
+    const imageList = document.createElement("li");
+    imageList.classList.add("gallery__image");
+    listGallery.append(imageList);
+    imageList.insertAdjacentHTML(
+        "beforeend",
+        `<a class="gallery__link"><img class="gallery__image" src=${image.preview} alt=${image.description}></></a>`
+    );
+
+    imageList.addEventListener("click", (event) => {
+        event.preventDefault();
+        const instance = basicLightbox.create(`<img src="${image.original}">`, {
+            onShow: (instance) => {
+                window.addEventListener("keyup", (event) => {
+                    if (event.code === "Escape") {
+                        instance.close();
+                    }
+                });
+            },
+        });
+
+        instance.show();
+    });
+});
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+console.log(galleryItems);
 
+const listGallery = document.querySelector(".gallery");
+
+galleryItems.forEach((image) => {
+    const imageList = document.createElement("li");
+    imageList.classList.add("gallery__image");
+    listGallery.append(imageList);
+    imageList.insertAdjacentHTML(
+        "beforeend",
+        `<a class="gallery__link" href=${image.original}><img class="gallery__image" src=${image.preview} alt=${image.description}></></a>`
+    );
+});
+
+const lightbox = new SimpleLightbox(".gallery a", {
+    captionDelay: 250,
+    captionsData: "alt",
+});
 console.log(galleryItems);
